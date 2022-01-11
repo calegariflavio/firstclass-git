53d1e698568ccac286078bcd5c60305d890e9261
calegariflavio <flavioescalegari@gmail.com>
2022-01-10 22:50:43 -0300

    Composition 3 class

---
+++ src/entities/Order.java
@@ -0,0 +1,87 @@
+package entities;
+
+import entities.enums.OrderStatus;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class Order {
+
+    private static final SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+    private Date moment;
+    private OrderStatus orderStatus;
+
+    private Client client;
+    private List<OrderItem> orderItems = new ArrayList<>();
+
+    public Order(Date moment, OrderStatus orderStatus, Client client) {
+        this.moment = moment;
+        this.orderStatus = orderStatus;
+        this.client = client;
+    }
+
+    public Date getMoment() {
+        return moment;
+    }
+
+    public void setMoment(Date moment) {
+        this.moment = moment;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return orderStatus;
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        this.orderStatus = orderStatus;
+    }
+
+    public Client getClient() {
+        return client;
+    }
+
+    public void setClient(Client client) {
+        this.client = client;
+    }
+
+    public List<OrderItem> getItems() {
+        return orderItems;
+    }
+
+    public void addItem(OrderItem orderItem) {
+        orderItems.add(orderItem);
+    }
+
+    public void removeItem(OrderItem orderItem) {
+        orderItems.remove(orderItem);
+    }
+
+    public double total() {
+        double sum = 0.0;
+        for (OrderItem i : orderItems) {
+                sum += i.subTotal();
+        }
+        return sum;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Order moment: ");
+        sb.append(sdf.format(moment) + "\n");
+        sb.append("Order status: ");
+        sb.append(orderStatus + "\n");
+        sb.append("Client: ");
+        sb.append(client + "\n");
+        sb.append("Order items:\n");
+        for (OrderItem item : orderItems) {
+            sb.append(item + "\n");
+        }
+        sb.append("Total price: $");
+        sb.append(String.format("%.2f", total()));
+        return sb.toString();
+    }
+}
---
+++ src/entities/OrderItem.java
@@ -0,0 +1,54 @@
+package entities;
+
+public class OrderItem {
+
+    private int quantity;
+    private double price;
+
+    private Product product;
+
+    public OrderItem(Integer quantity, Double price, Product product) {
+        this.quantity = quantity;
+        this.price = price;
+        this.product = product;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public double subTotal() {
+        return quantity * price;
+    }
+
+    @Override
+    public String toString() {
+        return product.getName()
+                + ", $"
+                + String.format("%.2f", price)
+                + ", Quantity: "
+                + quantity +
+                ", Subtotal: $"
+                + String.format("%.2f", subTotal());
+    }
+}
